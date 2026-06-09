159901ea87f299a6d98c32b352487d3e6a88ef38
Arthur Hipólito <hipolitoarthur3622@gmail.com>
2026-06-09 11:57:22 -0300

    feat: Adiciona background e arquitetura básica do jogo

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from turtle import *
 
+speed(0)
 distancia_movimento = 20
 
-cor_background("#CBBD93")
+##Cor da areia no background
+bgcolor("#CBBD93") 
+
+##Desenhando retângulo da areia e do mar, adicionando cor do mar e escondendo rastro da tartaruga
+penup()
+goto(100,200)
+pendown()
+color("blue")
+begin_fill()
+goto(300,200)
+goto(300,-200)
+goto(100,-200)
+goto(100,200)
+end_fill()
+
+done()
