796f7f302a324a469f61b0f1157b44880e8f1b7f
Arthur Hipólito <hipolitoarthur3622@gmail.com>
2026-06-09 12:08:27 -0300

    feat: Estiliza tartaruga e Cria seus movimentos

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from turtle import *
 speed(0)
 distancia_movimento = 20
 
-##Cor da areia no background
+##Cor da areia no background.
 bgcolor("#CBBD93") 
 
-##Desenhando retângulo da areia e do mar, adicionando cor do mar e escondendo rastro da tartaruga
+##Desenhando retângulo da areia e do mar, adicionando cor do mar e escondendo rastro da tartaruga.
 penup()
 goto(100,200)
 pendown()
@@ -17,5 +17,32 @@ goto(300,-200)
 goto(100,-200)
 goto(100,200)
 end_fill()
+
+##Cria a tartaruga e move ela ao centro do cenário.
+penup()
+goto(-160, 0)
+shape("turtle")
+color("green")
+
+##Cria movimentação da tartaruga
+def mover_cima():
+    setheading(90)
+    forward(distancia_movimento)
+    confirma_fim()
+
+def mover_baixo():
+    setheading(270)
+    forward(distancia_movimento)
+    confirma_fim()
+
+def mover_esquerda():
+    setheading(180)
+    forward(distancia_movimento)
+    confirma_fim()
+
+def mover_direita():
+    setheading(0)
+    forward(distancia_movimento)
+    confirma_fim()
 
 done()
